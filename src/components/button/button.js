a40c65d57e768cc9defd40912351f3c74f058c81
ilyas Macbook Air <ilyasmgazali@gmail.com>
2023-01-24 13:05:02 +0000

    jest project update

---
+++ src/components/button/button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './button.css';
+
+export default function Button({ label }) {
+  const [visible, setVisible] = React.useState(false);
+
+  return (
+    <>
+      <button
+        type="button"
+        className="button-style"
+        onClick={() => setVisible(true)}>
+        {label}
+      </button>
+      {!visible ? <p>Click me!</p> : <p>You clicked me!</p>}
+    </>
+  );
+}
